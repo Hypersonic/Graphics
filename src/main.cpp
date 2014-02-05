bf2276095e36544aba4df27dcb9855a8658431ce
Josh Hofing <joshhofing@gmail.com>
2014-02-05 11:02:47 -0500

    Update main to show pixel drawing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include "geometry/point.h"
+#include "canvas/canvas.h"
 
 int main(int argc, char** argv) {
   printf("Super cool stuff about graphics!\n");
-  Point2i v1 = Point2i();
-  Vec2i v2 = Vec2i(123, 421);
+  Canvas can = Canvas(640, 480);
+  can.clear();
 
-  int p = v1[0];
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 100; j++) {
+      can.putpixel(i, j, 255, 0, 0, 0);
+    }
+  }
+  can.render();
+
+  char buffer[256];
+  fgets(buffer, sizeof(buffer), stdin);
 
   return 0;
 }
