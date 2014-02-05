ada2289ffc6c10491579130bb286f53871e070d1
Josh Hofing <joshhofing@gmail.com>
2014-02-05 11:07:35 -0500

    Move line to it's own file

--- src/geometry/point.h
+++ src/geometry/point.h
@@ -32,16 +32,3 @@ public:
 
 };
 
-class Line {
-protected:
-  Vec2i _p1, _p2;
-public:
-  Line() : _p1(Vec2i()), _p2(Vec2i()) {}
-  Line(Vec2i p1, Vec2i p2) : _p1(p1), _p2(p2) {}
-
-  Vec2i  operator [](int index) const { return (&_p1)[index]; }
-  Vec2i& operator [](int index)       { return (&_p1)[index]; }
-
-  double length  () { return _p1.dist(_p2);  }
-  int    length2 () { return _p1.dist2(_p2); }
-};
