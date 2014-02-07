64185aa3d36533697cc7b935954f18e5cc4bda06
Josh Hofing <joshhofing@gmail.com>
2014-02-07 13:09:39 -0500

    Add area functions for rect and square.

--- src/geometry/rect.h
+++ src/geometry/rect.h
@@ -6,4 +6,6 @@ class Rect : public Quad {
 public:
   Rect(Vec2i topleft, Vec2i bottomright) :
     Quad(topleft, Vec2i(bottomright[0], topleft[1]), Vec2i(topleft[0], bottomright[1]), bottomright) {}
+
+  int area() { return _tl.dist(_tr)*_tl.dist(_bl); }
 };
--- src/geometry/square.h
+++ src/geometry/square.h
@@ -7,4 +7,6 @@ public:
   Square(Vec2i topleft, int length) : Rect(topleft, Vec2i(length, length) + topleft) {}
 
   int length() const { return Line(_tl, _tr).length(); }
+
+  int area() const { int l = length(); return l*l; }
 };
