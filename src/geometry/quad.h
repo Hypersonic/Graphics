c42007a8f7dd36a48683fd8e4dec7e6bf5393b7f
Josh Hofing <joshhofing@gmail.com>
2014-02-07 15:30:22 -0500

    Reorder initializer lists in Quad to squash -Wreorder warnings

--- src/geometry/quad.h
+++ src/geometry/quad.h
@@ -6,8 +6,8 @@ class Quad {
 protected:
   Vec2i _tl, _tr, _br, _bl;
 public:
-  Quad() : _tl(Vec2i()), _tr(Vec2i()), _bl(Vec2i()), _br(Vec2i()) {}
-  Quad(Vec2i topleft, Vec2i topright, Vec2i bottomleft, Vec2i bottomright) : _tl(topleft), _tr(topright), _bl(bottomleft), _br(bottomright) {}
+  Quad() : _tl(Vec2i()), _tr(Vec2i()), _br(Vec2i()), _bl(Vec2i()) {}
+  Quad(Vec2i topleft, Vec2i topright, Vec2i bottomleft, Vec2i bottomright) : _tl(topleft), _tr(topright), _br(bottomright), _bl(bottomleft) {}
 
   Vec2i  operator [](int index) const { return (&_tl)[index]; }
   Vec2i& operator [](int index)       { return (&_tl)[index]; }
