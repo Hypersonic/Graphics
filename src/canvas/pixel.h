dab1012751bab6d4f0d418cc2a7ac319c0d148f1
Josh Hofing <joshhofing@gmail.com>
2014-02-10 11:06:51 -0500

    Add Pixel class

---
+++ src/canvas/pixel.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "color.h"
+#include "geometry/point.h"
+
+class Pixel {
+protected:
+  Vec2i _pt;
+  Color _col;
+public:
+  Pixel() : _pt(Vec2i()), _col(Color()) {}
+  Pixel(Vec2i pt, Color col) : _pt(pt), _col(col) {}
+
+  Vec2i  point() const { return &_pt; }
+  Vec2i& point()       { return &_pt; }
+
+  Color  color() const { return &_col; }
+  Color& color()       { return &_col; }
+
+  Pixel operator + (Pixel& pixel) const { return Pixel(_pt + pixel._pt, _col + pixel._col); }
+  Pixel operator - (Pixel& pixel) const { return Pixel(_pt - pixel._pt, _col - pixel._col); }
+  
+  void operator += (Pixel& pixel)       { _pt += pixel._pt; _col += pixel._col;          }
+  void operator -= (Pixel& pixel)       { _pt -= pixel._pt; _col -= pixel._col;          }
+  bool operator == (Pixel& pixel)       { return _pt == pixel._pt && _col == pixel._col; }
+  bool operator != (Pixel& pixel)       { return _pt != pixel._pt || _col != pixel._col; }
+}
