db169bd4d0c9bdf30386a0d28356d09d76caae87
Josh Hofing <joshhofing@gmail.com>
2014-02-11 11:57:01 -0500

    Move debug function into a cpp file

---
+++ src/debug.cpp
@@ -0,0 +1,10 @@
+#include "debug.h"
+
+
+void DEBUG_REAL(std::string filename, int line, std::string function_name, std::string format, ...) {
+  va_list args;
+  va_start(args, format);
+  fprintf(stderr, "[%s, %i] %s:\n\t", filename.c_str(), line, function_name.c_str());
+  vfprintf(stderr, format.c_str(), args);
+  va_end(args);
+}
--- src/debug.h
+++ src/debug.h
@@ -5,10 +5,4 @@
 #include <string>
 
 #define debug(format, args...) DEBUG_REAL(__FILE__, __LINE__, __PRETTY_FUNCTION__, format, ## args)
-void DEBUG_REAL(std::string filename, int line, std::string function_name, std::string format, ...) {
-  va_list args;
-  va_start(args, format);
-  fprintf(stderr, "[%s, %i] %s:\n\t", filename.c_str(), line, function_name.c_str());
-  vfprintf(stderr, format.c_str(), args);
-  va_end(args);
-}
+void DEBUG_REAL(std::string filename, int line, std::string function_name, std::string format, ...);
