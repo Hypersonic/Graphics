fef16ccd9604a19e1a54712ea5735890fc7884b2
Josh Hofing <joshhofing@gmail.com>
2014-02-12 08:35:20 -0500

    Initialize Color with no set alpha to full opaque, not transparent. Stupid me.

--- src/canvas/color.h
+++ src/canvas/color.h
@@ -6,8 +6,8 @@ class Color {
 protected:
   int _r, _g, _b, _a;
 public:
-  Color()                           : _r(0), _g(0), _b(0), _a(0) {}
-  Color(int r, int g, int b)        : _r(r), _g(g), _b(b), _a(0) {}
+  Color()                           : _r(0), _g(0), _b(0), _a(255) {}
+  Color(int r, int g, int b)        : _r(r), _g(g), _b(b), _a(255) {}
   Color(int r, int g, int b, int a) : _r(r), _g(g), _b(b), _a(a) {}
   int  operator [] (int index) const { return (&_r)[index]; }
   int& operator [] (int index)       { return (&_r)[index]; }
