ba33d673efa685fa7e5f530e1c9d6f11f9f18d06
Josh Hofing <joshhofing@gmail.com>
2014-04-02 16:18:03 -0400

    Add 3d triangle type

--- src/geometry/tri.h
+++ src/geometry/tri.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "point.h"
+
 namespace TwoDee {
 class Triangle {
 protected:
@@ -14,3 +16,17 @@ public:
 };
 typedef Triangle Tri;
 }
+
+// 3d triangle
+class Triangle {
+protected:
+  Vec4f _v1, _v2, _v3;
+public:
+  Triangle() : _v1(Vec4f()), _v2(Vec4f()), _v3(Vec4f()) {}
+  Triangle(const Vec4f v1, const Vec4f v2, const Vec4f v3) : _v1(v1), _v2(v2), _v3(v3) {}
+  Triangle(Vec4f& v1, Vec4f& v2, Vec4f& v3) : _v1(v1), _v2(v2), _v3(v3) {}
+
+  Vec4f  operator[](int index) const { return (&_v1)[index]; }
+  Vec4f& operator[](int index)       { return (&_v1)[index]; }
+};
+typedef Triangle Tri;
